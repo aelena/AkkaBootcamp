c645271b124771437882beb9291d2dedc27a53cc
antonio elena <antonioelena@outlook.com>
2018-08-23 15:48:46 +0200

    first commit with initial implementations

---
+++ Wintail/WinTail/ConsoleReaderActor.cs
@@ -0,0 +1,43 @@
+﻿
+namespace WinTail
+{
+
+    using Akka.Actor;
+
+    using System;
+
+
+    internal class ConsoleReaderActor : UntypedActor
+    {
+        public const string ExitCommand = "exit";
+        public const string continueCommand = "continue";
+
+        private readonly IActorRef _consoleWriterActor;
+
+        /// <summary>
+        /// receives a reference to the ohter actor this actor has to communicate with.
+        /// </summary>
+        /// <param name="consoleWriterActor"></param>
+        public ConsoleReaderActor(IActorRef consoleWriterActor) => this._consoleWriterActor = consoleWriterActor;
+
+        /// <summary>
+        /// Defines what to do upon receiving a message.
+        /// </summary>
+        /// <param name="message"></param>
+        protected override void OnReceive(object message)
+        {
+            var consoleInput = Console.ReadLine();
+            if(!String.IsNullOrEmpty(consoleInput) && String.Equals(consoleInput, ExitCommand, StringComparison.OrdinalIgnoreCase))
+            {
+                // shut down the system via the Akka Context
+                Context.System.Terminate();
+                return;
+            }
+
+            // else tell the other actor
+            this._consoleWriterActor.Tell(consoleInput);
+
+            Self.Tell(continueCommand);
+        }
+    }
+}
---
+++ Wintail/WinTail/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace WinTail
+{
+    using System;
+
+    using Akka.Actor;
+
+    class Program
+    {
+
+        private static ActorSystem wintailActorSystem;
+
+        static void Main(string[] args)
+        {
+            wintailActorSystem = ActorSystem.Create("WintailActorSys");
+
+            PrintInstructions();
+
+            CreateActors();
+
+            wintailActorSystem.WhenTerminated.Wait();
+        }
+
+        private static void CreateActors()
+        {
+            CreateReaderActor();
+        }
+        
+        private static void CreateReaderActor() =>
+            wintailActorSystem.ActorOf(Props.Create(() => new ConsoleReaderActor(CreateWriterActor())), "consoleReader").Tell(("start"));
+
+
+        private static IActorRef CreateWriterActor() =>
+            wintailActorSystem.ActorOf(Props.Create(() => new ConsoleWriterActor()), "consoleWriter");
+
+
+        private static void PrintInstructions()
+        {
+            Console.WriteLine("Write whatever you want into the console!");
+            Console.Write("Some lines will appear as");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.Write(" red ");
+            Console.ResetColor();
+            Console.Write(" and others will appear as");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(" green! ");
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Type 'exit' to quit this application at any time.\n");
+        }
+    }
+}
